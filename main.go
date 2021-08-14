ce9d89299f8335e3f2a9893dc03457c5ccb05f06
HeadBangZ <thomasnhprivat@gmail.com>
2021-08-14 19:44:13 +0200

    Change generating password! Need to change math/rand with crypto/rand

--- main.go
+++ main.go
@@ -1,41 +1,61 @@
 package main
 
 import (
+	//"crypto/rand"
 	"fmt"
+	"math/rand"
 	"strconv"
 )
 
-// Make these into slices?
-// TODO: Make these into slices
-const LOWERCASE = "abcdefghijklmnopqrstunwxyz"
-const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const NUMBERS = "0123456789"
-const SYMBOLS = "!\"#$%&'()*+,-.:;<>"
+const (
+	LOWERCASE = "abcdefghijklmnopqrstunwxyz"
+	UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	NUMBERS   = "0123456789"
+	SYMBOLS   = "~!@#$%^&*()_+`-={}|[]\\:\"<>?,./"
+)
 
 func main() {
 	fmt.Println("How long you want your password?")
 	fmt.Println("-------------------")
-
 	length := getUserInput()
-	fmt.Printf("%d\n", length)
 
-	generatePassword(length)
+	password := generatePassword(length)
+	fmt.Println(password)
 }
 
-func getUserInput() int {
+func getUserInput() *int {
 	var input string
-	fmt.Scanf("%s", &input)
+	fmt.Scanln(&input)
 	length, err := strconv.Atoi(input)
 	if err != nil {
 		fmt.Println("Input has to be a number!")
 		getUserInput()
 	}
 
-	return length
+	if length < 8 {
+		fmt.Println("Length of password needs to be at least 8")
+		getUserInput()
+	}
+
+	return &length
 }
 
-func generatePassword(n int) string {
-	fmt.Printf("%s\n", fmt.Sprintf("Generate password: %d is the chosen length", n))
+// TODO: change to crypto/rand instead of math/rand, math/rand ain't secure to use.
+func generatePassword(length *int) string {
+	fmt.Printf("%s\n", fmt.Sprintf("Generate password: %d is the chosen length", *length))
+	chars := UPPERCASE + LOWERCASE + NUMBERS + SYMBOLS
+	pw := make([]byte, *length)
+	pw[0] = UPPERCASE[rand.Intn(len(UPPERCASE))]
+	pw[1] = LOWERCASE[rand.Intn(len(LOWERCASE))]
+	pw[2] = NUMBERS[rand.Intn(len(NUMBERS))]
+	pw[3] = SYMBOLS[rand.Intn(len(SYMBOLS))]
+	for i := 4; i <= *length-1; i++ {
+		pw[i] = chars[rand.Intn(len(chars))]
+	}
 
-	return ""
+	rand.Shuffle(len(pw), func(i, j int) {
+		pw[i], pw[j] = pw[j], pw[i]
+	})
+
+	return string(pw)
 }
